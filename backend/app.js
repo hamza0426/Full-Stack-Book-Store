4f1470917b4e92e812a421d182420094b0596e1a
hamza0426 <muhammadhamzaowais5@gmail.com>
2025-02-12 03:21:45 +0500

    Database Connected

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+require("./connection/conn");
 
+// app.get("/", (req, res) => {
+//     res.send("Hello from Backend...");
+// });
 
-app.listen(1000, () => {
-    console.log("Server Started");
+app.listen(process.env.PORT, () => {
+    console.log(`Server Started at ${process.env.PORT}`);
 });
