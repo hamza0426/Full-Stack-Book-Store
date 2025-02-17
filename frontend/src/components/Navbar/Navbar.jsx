a9aceafbb7b1cf55692dd76e66eccb267faf3ce4
Fahadkhan1503 <Fahadkhan1503@gmail.com>
2025-02-17 13:04:45 +0500

    hamburger hidden

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -25,80 +25,85 @@ const Navbar = () => {
       link: "/profile",
     },
   ];
-  const [Mblnav, setMblnav] = useState("hidden")
+  const [Mblnav, setMblnav] = useState("hidden");
 
   const toggleMblnav = () => {
     setMblnav((prev) => (prev === "hidden" ? "block" : "hidden"));
   };
   return (
     <>
-    <nav className="z-50 relative flex bg-zinc-800 text-white px-8 py-2 items-center justify-between">
-      <Link to={"/"} className=" flex items-center">
-        <img
-          className="h-10 me-4"
-          src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
-          alt="logo"
-        />
-        <h1 className="text-2xl font-semibold">FahamBooks</h1>
-      </Link>
-      <div className="nav-links-fahambooks block md:flex items-center gap-4 transition-all duration-300 cursor-pointer">
-        <div className="hidden md:flex gap-4">
-          {links.map((items, i) => (
+      <nav className="z-50 relative flex bg-zinc-800 text-white px-8 py-2 items-center justify-between">
+        <Link to={"/"} className=" flex items-center">
+          <img
+            className="h-10 me-4"
+            src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
+            alt="logo"
+          />
+          <h1 className="text-2xl font-semibold">FahamBooks</h1>
+        </Link>
+        <div className="nav-links-fahambooks block md:flex items-center gap-4 transition-all duration-300 cursor-pointer">
+          <div className="hidden md:flex gap-4">
+            {links.map((items, i) => (
+              <Link
+                to={items.link}
+                className="hover:text-blue-500 transition-all duration-300"
+                key={i}
+              >
+                {items.title}{" "}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden md:flex gap-4">
             <Link
-              to={items.link}
-              className="hover:text-blue-500 transition-all duration-300"
-              key={i}
+              to={"/login"}
+              className="px-4 py-1 border-[var(--color-button)] border rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
             >
-              {items.title}{" "}
+              Login
             </Link>
-          ))}
-        </div>
-        <div className="hidden md:flex gap-4">
-          <Link
-            to={"/login"}
-            className="px-4 py-1 border-[var(--color-button)] border rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+            <Link
+              to={"/sign-up"}
+              className="px-4 py-1 bg-button rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+            >
+              Sign Up
+            </Link>
+          </div>
+          <button
+            className="block md:hidden text-white text-2xl hover:text-zinc-500"
+            onClick={toggleMblnav}
           >
-            Login
-          </Link>
-          <Link
-            to={"/sign-up"}
-            className="px-4 py-1 bg-button rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
-          >
-            Sign Up
-          </Link>
+            <MdOutlineSegment />
+          </button>
         </div>
-        <button className="text-white text-2xl hover:text-zinc-500"
-        onClick={toggleMblnav}>
-        <MdOutlineSegment />
-        </button>
+      </nav>
+      <div
+        className={`${Mblnav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center`}
+      >
+        {links.map((items, i) => (
+          <Link
+            to={items.link}
+            className={`${Mblnav} text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300`}
+            key={i}
+            onClick={toggleMblnav}
+          >
+            {items.title}{" "}
+          </Link>
+        ))}
+
+        <Link
+          to={"/login"}
+          className={`${Mblnav} px-8 mb-8 text-3xl font-semibold border-[var(--color-button)] border rounded text-white hover:bg-white hover:text-zinc-800 transition-all duration-300`}
+          onClick={toggleMblnav}
+        >
+          Login
+        </Link>
+        <Link
+          to={"/sign-up"}
+          className={`${Mblnav} px-8 mb-8 text-3xl font-semibold bg-button rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}
+          onClick={toggleMblnav}
+        >
+          Sign Up
+        </Link>
       </div>
-    </nav>
-    <div className={`${Mblnav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center`}>
-    {links.map((items, i) => (
-            <Link
-              to={items.link}
-              className="text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300"
-              key={i}
-              onClick={toggleMblnav}
-            >
-              {items.title}{" "}
-            </Link>
-          ))}
-          
-          <Link
-            to={"/login"}
-            className={`${Mblnav} px-8 mb-8 text-3xl font-semibold border-[var(--color-button)] border rounded text-white hover:bg-white hover:text-zinc-800 transition-all duration-300`}
-          >
-            Login
-          </Link>
-          <Link
-            to={"/sign-up"}
-            className={`${Mblnav} px-8 mb-8 text-3xl font-semibold bg-button rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}
-          >
-            Sign Up
-          </Link>
-        
-    </div>
     </>
   );
 };
