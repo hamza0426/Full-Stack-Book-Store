652db761360ddfb7df403ede4efe4f235d384e59
Muhammad Hamza Owais <Muhammadhamzaowais5@gmail.com>
2026-04-22 20:13:04 +0500

    Everything fixed

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -17,9 +17,9 @@ const BookCard = ({ data, favourite }) => {
     );
     alert(response.data.message);
   };
-// bg-[#EFE7DA]
+
   return (
-    <div className="h-full bg-[#FF0000] backdrop-blur-lg rounded-lg p-6 flex flex-col shadow-xl border border-[#D8C3A5] text-[#4A3F35]">
+    <div className="h-full bg-[#EFE7DA] backdrop-blur-lg rounded-lg p-6 flex flex-col shadow-xl border border-[#D8C3A5] text-[#4A3F35]">
       <Link to={`/view-book-details/${data._id}`} className="flex flex-col flex-grow">
         <div>
           <div className="h-64 bg-zinc-700 rounded flex items-center justify-center overflow-hidden shadow-md">
